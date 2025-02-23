343380745f4e118113eb9741ac8a2c03cd0cff90
Lan Anh <dhlananh2309@gmail.com>
2025-02-23 15:17:52 +0700

    upload folder src/constants/

---
+++ src/constants/images.ts
@@ -0,0 +1,13 @@
+import memojiImage from "@/assets/images/memoji-computer.png"
+import grainImage from "@/assets/images/grain.jpg"
+import ArrowDown from "@/assets/icons/arrow-down.svg"
+import StarIcon from "@/assets/icons/star.svg"
+import SparkleIcon from "@/assets/icons/sparkle.svg"
+
+export const Images = {
+  memojiImage,
+  grainImage,
+  ArrowDown,
+  StarIcon,
+  SparkleIcon,
+};
