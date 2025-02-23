9c69130bb6881f1b1e1dd626cd304e4f42317735
Lan Anh <dhlananh2309@gmail.com>
2025-02-23 15:17:30 +0700

    upload folder src/sections/

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,77 @@
+import React from "react"
+import Image from "next/image"
+import { Images } from "@/constants/images"
+import { HeroOrbit } from "@/components/HeroOrbit";
+
+export const HeroSection = () => {
+  return (
+    <div className="py-32 md:py-48 lg:py-60 relative z-0">
+      <div
+        className="absolute inset-0 -z-30 opacity-5"
+        style={{
+          backgroundImage: `url(${Images.grainImage})`,
+        }}
+      />
+      <div className="size-[620px] hero-ring" />
+      <div className="size-[820px] hero-ring" />
+      <div className="size-[1020px] hero-ring" />
+      <div className="size-[1220px] hero-ring" />
+      <HeroOrbit size={800} rotation={-72}>
+        <Images.StarIcon className="size-28 text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size={550} rotation={20}>
+        <Images.StarIcon className="size-12 text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size={590} rotation={98}>
+        <Images.StarIcon className="size-8 text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size={430} rotation={-14}>
+        <Images.StarIcon className="size-8 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={430} rotation={79}>
+        <Images.StarIcon className="size-5 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={530} rotation={178}>
+        <Images.StarIcon className="size-10 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={710} rotation={144}>
+        <Images.StarIcon className="size-14 text-emerald-300/20" />
+      </HeroOrbit>
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image
+            src={Images.memojiImage}
+            className="size-[100px]"
+            alt="Person peeking from behind laptop"
+          />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full" />
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
+          </div>
+        </div>
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Building Exceptional User Experiences
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg ">
+            I specialize in transforming designs into functional,
+            high-performing web applications. Let's discuss your next project
+            and bring your ideas to life.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Explore My Work</span>
+            <Images.ArrowDown className="size-4" />
+          </button>
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl">
+            <span>👋🏻</span>
+            <span className="font-semibold">Let's Connect</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
