bd3c14f5c3ef0cd4b5a966ad4861f93a3676ca1a
Lan Anh <dhlananh2309@gmail.com>
2025-02-23 15:18:41 +0700

    upload folder src/components/ & commit code `HeroOrbits`

---
+++ src/components/HeroOrbit.tsx
@@ -0,0 +1,30 @@
+import { Images } from "@/constants/images"
+import { PropsWithChildren } from "react"
+
+export const HeroOrbit = ({ 
+  children, 
+  size, 
+  rotation 
+}: PropsWithChildren<{ size: number, rotation: number; }>) => {
+  return (
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div
+        className="rotate-45"
+        style={{
+          height: `${size}px`,
+          width: `${size}px`,
+          transform: `rotate(${rotation}deg)`,
+        }}
+      >
+        <div 
+          className="inline-flex"
+          style={{
+            transform: `rotate(${rotation * -1}deg)`,
+          }}
+        >
+            {children}
+        </div>
+      </div>
+    </div>
+  )
+}
