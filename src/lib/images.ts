93b4b429c1c625b87c4e7309ce315f680b7c09b2
Lan Anh <dhlananh2309@gmail.com>
2025-08-24 00:25:04 +0700

    feat: import project thumbnail and gallery images

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,5 +1,45 @@
 import { StaticImageData } from "next/legacy/image";
 
+// --- Project Thumbnails & Galleries ---
+import BookstoreThumbnail from "../../public/images/projects/bookstore-project-thumbnail.png";
+import BookstoreHomepage1 from "../../public/images/projects/bookstore-project-homepage-1.png";
+import BookstoreHomepage2 from "../../public/images/projects/bookstore-project-homepage-2.png";
+import BookstoreHomepage3 from "../../public/images/projects/bookstore-project-homepage-3.png";
+import BookstoreCategory1 from "../../public/images/projects/bookstore-project-category-astronaut-1.png";
+import BookstoreCategory2 from "../../public/images/projects/bookstore-project-category-astronaut-2.png";
+import BookstoreCategory3 from "../../public/images/projects/bookstore-project-category-astronaut-3.png";
+import BookstoreBackend from "../../public/images/projects/bookstore-project-backend.png";
+import BookstoreChatbot from "../../public/images/projects/bookstore-chatbot-project-thumbnail.png";
+
+import ChatHubWeb from "../../public/images/projects/chathub-web.png";
+
+import ForeverBuyThumbnail from "../../public/images/projects/foreverbuy-ecommerce-project-thumbnail.png";
+import ForeverBuyHomepage1 from "../../public/images/projects/foreverbuy-ecommerce-project-homepage-1.png";
+import ForeverBuyHomepage2 from "../../public/images/projects/foreverbuy-ecommerce-project-homepage-2.png";
+import ForeverBuyHomepage3 from "../../public/images/projects/foreverbuy-ecommerce-project-homepage-3.png";
+import ForeverBuyHomepage4 from "../../public/images/projects/foreverbuy-ecommerce-project-homepage-4.png";
+import ForeverBuyCollection1 from "../../public/images/projects/foreverbuy-ecommerce-project-collection-page-1.png";
+import ForeverBuyCollection2 from "../../public/images/projects/foreverbuy-ecommerce-project-collection-page-2.png";
+import ForeverBuyCollection3 from "../../public/images/projects/foreverbuy-ecommerce-project-collection-page-3.png";
+import ForeverBuyCollection4 from "../../public/images/projects/foreverbuy-ecommerce-project-collection-page-4.png";
+import ForeverBuyCollection5 from "../../public/images/projects/foreverbuy-ecommerce-project-collection-page-5.png";
+import ForeverBuyLogin from "../../public/images/projects/foreverbuy-ecommerce-project-login-page.png";
+import ForeverBuyCart from "../../public/images/projects/foreverbuy-ecommerce-project-cart-page.png";
+import ForeverBuyDetails from "../../public/images/projects/foreverbuy-ecommerce-project-product-details-page.png";
+
+import PortfolioThumbnail from "../../public/images/projects/portfolio-website-thumbnail.png";
+import PortfolioCareerGoals from "../../public/images/projects/portfolio-website-career-goals.png";
+import PortfolioProjects from "../../public/images/projects/portfolio-website-projects-section.png";
+import PortfolioSkills from "../../public/images/projects/portfolio-website-skills-section.png";
+import PortfolioGithub from "../../public/images/projects/portfolio-website-github-contributions-section.png";
+import PortfolioTestimonials from "../../public/images/projects/portfolio-website-testimonials-section.png";
+import PortfolioFAQ from "../../public/images/projects/portfolio-website-faq-section.png";
+import PortfolioResume from "../../public/images/projects/portfolio-website-resume-section.png";
+import PortfolioContact from "../../public/images/projects/portfolio-website-contact-section.png";
+import PortfolioDetails1 from "../../public/images/projects/portfolio-website-project-details-page-1.png";
+import PortfolioDetails2 from "../../public/images/projects/portfolio-website-project-details-page-2.png";
+import PortfolioDetails3 from "../../public/images/projects/portfolio-website-project-details-page-3.png";
+
 // --- Testimonial Avatars ---
 import JaneDoeAvatar from "../../public/images/avatars/jane-doe.png";
 import JohnSmithAvatar from "../../public/images/avatars/john-smith.png";
@@ -16,6 +56,49 @@ import DefaultAvatar from "../../public/images/avatars/default-avatar.png";
 
 // --- Export Object ---
 export const Images: { [ key: string ]: StaticImageData } = {
+  // Bookstore Project
+  BookstoreThumbnail,
+  BookstoreHomepage1,
+  BookstoreHomepage2,
+  BookstoreHomepage3,
+  BookstoreCategory1,
+  BookstoreCategory2,
+  BookstoreCategory3,
+  BookstoreBackend,
+  BookstoreChatbot,
+
+  // ChatHub Project
+  ChatHubWeb,
+
+  // ForeverBuy Project
+  ForeverBuyThumbnail,
+  ForeverBuyHomepage1,
+  ForeverBuyHomepage2,
+  ForeverBuyHomepage3,
+  ForeverBuyHomepage4,
+  ForeverBuyCollection1,
+  ForeverBuyCollection2,
+  ForeverBuyCollection3,
+  ForeverBuyCollection4,
+  ForeverBuyCollection5,
+  ForeverBuyLogin,
+  ForeverBuyCart,
+  ForeverBuyDetails,
+
+  // Portfolio Project
+  PortfolioThumbnail,
+  PortfolioCareerGoals,
+  PortfolioProjects,
+  PortfolioSkills,
+  PortfolioGithub,
+  PortfolioTestimonials,
+  PortfolioFAQ,
+  PortfolioResume,
+  PortfolioContact,
+  PortfolioDetails1,
+  PortfolioDetails2,
+  PortfolioDetails3,
+
   // Avatars
   JaneDoeAvatar,
   JohnSmithAvatar,
